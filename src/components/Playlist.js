abc57f9cadf12fca98d0f5023044579d8d1128fb
Medet Kenenbayev <medetkenenbayev@Medets-MacBook-Pro.local>
2024-10-23 00:58:19 -0400

    second try

---
+++ src/components/Playlist.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Playlist() {
+  return <div className="Playlist"></div>;
+}
+
+export default Playlist;
---
+++ src/components/Tracklist.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Tracklist() {
+  return <div className="Tracklist"></div>;
+}
+
+export default Tracklist;
